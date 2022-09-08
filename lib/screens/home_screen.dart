b9007053d89a0cbefe5a6641892f85e14069d785
Denis <jaddev888@gmail.com>
2022-09-08 23:47:24 +0300

    add bottomNavigationBar

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xFFeeedf2),
+      body: Column(),
+    );
+  }
+}
